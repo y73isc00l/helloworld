57882da209fdd03caf632a966215c03932b76be3
y73isc00l <rachittibrewal@gmail.com>
2017-01-03 17:32:29 +0530

    Adding python module for flattening lists finalver2.0

--- flatten.py
+++ flatten.py
@@ -1,19 +1,12 @@
-def checkdeflation(lst):
-  for elem in lst:
-    if isinstance(elem,list): return False
-  return True
-
+#!/usr/bin/python2
 def my_flatten(lst):
-  if checkdeflation(lst)==True:return lst
-  else:
-    count=len(lst)
-    for i in range(count):
-      temp=lst[-1]
-      temp1=[]
-      del lst[-1]
-      if isinstance(temp,(int,float,str)):
-          temp1.append(temp)
-          lst.extend(temp1)
-      else: lst.extend(temp)
-    return my_flatten(lst)
+  count=len(lst)
+  for i in range(count):
+    elem=lst[0]
+    del lst[0]
+    if not isinstance(elem,list):
+      yield elem
+    else:
+      for childelem in my_flatten(elem):
+        yield childelem
 
